cbaa53c24bb2aae66067b0eebcde9b928f7c64a7
Muhammad Thariq Hadad <thariq.hadad@gmail.com>
2023-12-08 23:17:57 +0700

    Customizing styles using 'sx' from material-ui

--- src/MyUrlField.tsx
+++ src/MyUrlField.tsx
@@ -1,10 +1,16 @@
 // in src/MyUrlField.tsx
 import { useRecordContext } from "react-admin";
+import { Link } from "@mui/material";
+import LaunchIcon from "@mui/icons-material/Launch";
 
 const MyUrlField = ({ source }: { source: string }) => {
   const record = useRecordContext();
-  if (!record) return null;
-  return <a href={record[source]}>{record[source]}</a>;
+  return record ? (
+    <Link href={record[source]} sx={{ textDecoration: "none" }}>
+      {record[source]}
+      <LaunchIcon sx={{ fontSize: 15, ml: 1 }} />
+    </Link>
+  ) : null;
 };
 
 export default MyUrlField;

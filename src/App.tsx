73df1698de0577f24818b85f7e7ce42b24feeb89
Muhammad Thariq Hadad <thariq.hadad@gmail.com>
2023-12-08 22:57:19 +0700

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import { Admin, Resource } from "react-admin";
+import { dataProvider } from "./dataProvider";
+import { UserList } from "./users";
+
+export const App = () => <Admin dataProvider={dataProvider}>
+  <Resource name="users" list={UserList} />
+</Admin>;

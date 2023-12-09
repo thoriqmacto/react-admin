4fe9c2d0c891a98107a7cc92fb4215a98002f32d
Muhammad Thariq Hadad <thariq.hadad@gmail.com>
2023-12-09 12:14:25 +0700

    Adding dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import { dataProvider } from "./dataProvider";
 import { PostList, PostEdit, PostCreate } from "./posts";
 import { UserList } from "./users";
 import { UserShow } from "./userShow";
+import PostIcon from "@mui/icons-material/Book";
+import UserIcon from "@mui/icons-material/Group";
+import { Dashboard } from "./Dashboard";
 
 export const App = () => (
-  <Admin dataProvider={dataProvider}>
-    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />    
-    <Resource name="users" list={UserList} show={UserShow} recordRepresentation="name" />
+  <Admin dataProvider={dataProvider} dashboard={Dashboard}>
+    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />    
+    <Resource name="users" list={UserList} show={UserShow} recordRepresentation="name" icon={UserIcon} />
   </Admin>
 );

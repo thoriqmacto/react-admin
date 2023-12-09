32fdce180f64f060103db79e9016d5fc5d1092f8
Muhammad Thariq Hadad <thariq.hadad@gmail.com>
2023-12-09 12:32:48 +0700

    Adding authProvider

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,10 @@ import { UserShow } from "./userShow";
 import PostIcon from "@mui/icons-material/Book";
 import UserIcon from "@mui/icons-material/Group";
 import { Dashboard } from "./Dashboard";
+import { authProvider } from "./authProvider";
 
 export const App = () => (
-  <Admin dataProvider={dataProvider} dashboard={Dashboard}>
+  <Admin authProvider={authProvider} dataProvider={dataProvider} dashboard={Dashboard}>
     <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />    
     <Resource name="users" list={UserList} show={UserShow} recordRepresentation="name" icon={UserIcon} />
   </Admin>

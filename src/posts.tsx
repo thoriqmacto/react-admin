8e0483e7e33cc9ab4937472f7d5349421c264b52
Muhammad Thariq Hadad <thariq.hadad@gmail.com>
2023-12-09 12:09:36 +0700

    Adding search and filter

--- src/posts.tsx
+++ src/posts.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-key */
 import {
   List,
   Datagrid,
@@ -17,8 +18,13 @@ const PostTitle = () => {
   return <span>Post {record ? `"${record.title}"` : ''}</span>;
 };
 
+const postFilters = [
+  <TextInput source="q" label="Search" alwaysOn />,
+  <ReferenceInput source="userId" label="User" reference="users" />
+];
+
 export const PostList = () => (
-  <List>
+  <List filters={postFilters} >
     <Datagrid>
       <TextField source="id" />
       <ReferenceField source="userId" reference="users" link="show" />

166c3a64eef783ef946e0f1f6519b94e4f1327c8
Muhammad Thariq Hadad <thariq.hadad@gmail.com>
2023-12-09 11:13:45 +0700

    Trying relationship with ReferenceField

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { Admin, Resource, ListGuesser } from "react-admin";
+import { Admin, Resource } from "react-admin";
 import { dataProvider } from "./dataProvider";
+import { PostList } from "./posts";
 import { UserList } from "./users";
 
 export const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="users" list={UserList} />
+    <Resource name="posts" list={PostList} />
+    <Resource name="users" list={UserList} recordRepresentation="name" />
   </Admin>
 );

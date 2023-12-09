c91b5e87839a66f635a3976124535d468bc89267
Muhammad Thariq Hadad <thariq.hadad@gmail.com>
2023-12-09 11:52:57 +0700

    Add edit capabilities

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Admin, Resource } from "react-admin";
 import { dataProvider } from "./dataProvider";
-import { PostList } from "./posts";
+import { PostList, PostEdit } from "./posts";
 import { UserList } from "./users";
 import { UserShow } from "./userShow";
 
 export const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="posts" list={PostList} />    
+    <Resource name="posts" list={PostList} edit={PostEdit} />    
     <Resource name="users" list={UserList} show={UserShow} recordRepresentation="name" />
   </Admin>
 );
--- src/posts.tsx
+++ src/posts.tsx
@@ -1,4 +1,14 @@
-import { List, Datagrid, TextField, ReferenceField, EditButton } from "react-admin";
+import {
+  List,
+  Datagrid,
+  TextField,
+  ReferenceField,
+  EditButton,
+  Edit,
+  ReferenceInput,
+  SimpleForm,
+  TextInput,
+} from "react-admin";
 
 export const PostList = () => (
   <List>
@@ -9,4 +19,15 @@ export const PostList = () => (
       <EditButton />      
     </Datagrid>
   </List>
+);
+
+export const PostEdit = () => (
+  <Edit>
+    <SimpleForm>
+      <TextInput source="id" inputProps={{disabled:true}} />
+      <ReferenceInput source="userId" reference="users" />
+      <TextInput source="title" />      
+      <TextInput source="body" multiline rows={5} />
+    </SimpleForm>
+  </Edit>
 );
